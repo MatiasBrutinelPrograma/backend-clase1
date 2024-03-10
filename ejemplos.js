759e8633c04a341f8679f843cae9b974afd59f02
Matias Brutinel <131211925+MatiasBrutinelPrograma@users.noreply.github.com>
2024-03-10 15:20:04 -0300

    actualizando_clase2

---
+++ ejemplos.js
@@ -0,0 +1,28 @@
+// ES7 ->
+// exponencial (**)
+// includes -> arrays
+
+// const valor1 = 5;
+
+// console.log(5 * 5 * 5);
+// console.log(5 ** 3);
+
+// const nombres = ['Gabriel', 'Isaac', 'Joaquin'];
+
+// console.log(nombres.includes('isaac'));
+// console.log(nombres.includes('Isaac'));
+
+// ES8
+// async - await
+// Object.entries(), Object.values(), Object.Keys()
+
+const estudiante1 ={
+    nombre:'Joaquin',
+    apellido:'Capanegra',
+    edad:35,
+    barrio:'buenos aires'
+}
+
+// console.log(Object.entries(estudiante1));
+// console.log(Object.keys(estudiante1));
+console.log(Object.values(estudiante1));
